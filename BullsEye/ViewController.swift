404ee1799069384fd4972540f71fdb93f7213e28
Alberto Franco <alberto.franco86@qmail.cuny.edu>
2018-01-18 15:08:45 -0500

    Created a random number that is added to the label upon app load

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,14 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var targetNumberLabel: UILabel!
+    var targetNumber : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setTargetAndLabel();
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setTargetAndLabel() {
+        targetNumber = Int(arc4random_uniform(101));
+        let message = "Put the Bull's eye as close as you can to: "
+        targetNumberLabel.text = "\(message)\(targetNumber)"
     }
 
 
