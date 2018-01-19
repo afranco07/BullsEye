d77b75a173d84391eace9cb277671a442d47a5f9
Alberto Franco <alberto.franco86@qmail.cuny.edu>
2018-01-18 19:27:09 -0500

    Added dynamic label for round number

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var roundLabel: UILabel!
     @IBOutlet weak var targetNumberLabel: UILabel!
     var targetNumber : Int = 0
+    var roundNumber : Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         setTargetAndLabel();
+        setRoundNumber()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,10 +27,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setRandomTarget() {
+        targetNumber = Int(arc4random_uniform(101))
+    }
+    
     func setTargetAndLabel() {
-        targetNumber = Int(arc4random_uniform(101));
+        setRandomTarget()
         let message = "Put the Bull's eye as close as you can to: "
         targetNumberLabel.text = "\(message)\(targetNumber)"
+    }
+    
+    func setRoundNumber() {
+        let label = "Round: "
+        roundLabel.text = "\(label)\(roundNumber)"
     }
 
 
